8ffbad540413cdb51e8e2dd4ed74552bbf8f7e26
tenyiting <jaeseumin.yt@gmail.com>
2021-09-13 14:38:14 +0800

    chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    resources :posts
+  scope "(:locale)", locale: /en|ja/ do
     root 'static_pages#home', as: "home"
     get 'static_pages/help', as: "help"
 
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
-    resources :users, only: %i(:new, :create, :show)
+    resources :users, only: %i(new create show)
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 end
